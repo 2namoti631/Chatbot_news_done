13820a518513d127b51fc50eb1b9f6ae2b39e46e
Anh Nam dz <namhaii631@gmail.com>
2024-06-12 16:38:09 +0700

    update app.y hoàn thiện

--- app.py
+++ app.py
@@ -1,31 +1,51 @@
-import nltk
-import numpy as np
-import joblib
-import json
+import string
 import random
+import json
+import csv
+import joblib
+import numpy as np
 from flask import Flask, render_template, request
 from mtranslate import translate
 
-# Load mô hình phân loại câu hỏi
+# Load mô hình và vector hóa của phân loại câu hỏi
 model_detec = joblib.load('model_intent_detection.joblib')
 vectorizer_detec = joblib.load('vectorizer_detec.joblib')
+
+# load file data dữ liệu phân loại câu hỏi
 with open('label_question_detect.json', 'r', encoding='utf-8') as file:
     label_data = json.load(file)
 
-
-# Load mô hình hội thoại  
+# Load mô hình và vector hóa của việc training hội thoại  
 model_conver = joblib.load('model_conversation.joblib')
 vectorizer_conver = joblib.load('vectorizer_conver.joblib')
 
-
-# Load dữ liệu tin tức
+# Load dữ liệu tin tức đã được scrapt về
 with open('news_scrap_data.json', encoding='utf-8') as file:
     news_scrap_data = json.load(file)
 
-# Load dữ liệu intents từ file data.json
+# Load dữ liệu intents từ file hội thoại cơ bản cho chatbot
 with open('data_basic_conver.json', encoding='utf-8') as file:
     intents = json.load(file)
 
+
+# Chức năng đọc dữ liệu từ file csv của data hội thoại và các bước tiền xử lý để để có thể truy cập data
+def load_data(filename):
+    data = {}
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            tag = row['Tag']
+            if tag not in data:
+                data[tag] = []
+            data[tag].append({
+                'Pattern': row['Pattern'].translate(str.maketrans('', '', string.punctuation)).lower(),
+                'Response': row['Response']
+            })
+    return data
+
+data_conversation = load_data('data_conver.csv')
+
+# Chức năng phân loại chủ đề câu hỏi
 def classify_intent_questions(msg):
     msg = translate(msg, "en")
     data_processing = vectorizer_detec.transform([msg]).toarray()
@@ -33,9 +53,10 @@ def classify_intent_questions(msg):
     x = predict[0]
     result = get_definition_and_trans(x)
     if result:
-        return f" <strong>Chủ đề:</strong> {result['translation']}<br> \n <strong>Thuộc chủ đề lớn: </strong> {result['definition']}<br>"
+        return f"<strong>Chủ đề:</strong> {result['translation']}<br> \n <strong>Thuộc chủ đề lớn: </strong> {result['definition']}<br>"
     return "Không thể xác định chủ đề."
 
+# chức năng để truy cập vào file json và lấy các biến liên quan đến câu hỏi được dự đoán để in ra. (Phân loại câu hỏi)
 def get_definition_and_trans(word):
     result = {}
     for category, values in label_data.items():
@@ -46,29 +67,29 @@ def get_definition_and_trans(word):
                 return result
     return None
 
-def classify_intent(msg):
+# Chức năng trả lời câu hỏi hội thoại theo model training
+def get_response_from_model(msg, model_conver, vectorizer_conver, intents):
+    bow = vectorizer_conver.transform([msg])
+    predicted_tag = model_conver.predict(bow)[0]
     for intent in intents['intents']:
-        for pattern in intent['patterns']:
-            if pattern.lower() in msg.lower():
-                return intent
-    return None
- 
+        if intent['tag'] == predicted_tag:
+            return random.choice(intent['responses'])
+    return "Xin lỗi, tôi không hiểu câu hỏi của bạn."
 
-def chatbot_response(msg):
-    categories = ["Thời sự", "Góc nhìn", "Thế giới", "Podcasts", "Kinh doanh", "Bất động sản", "Khoa học", 
-                  "Giải trí", "Thể thao", "Pháp luật", "Giáo dục", "Sức khỏe", "Đời sống", "Du lịch", 
-                  "Số hóa", "Xe", "Ý kiến", "Tâm sự", "Thư giãn"]
-
-    intent = classify_intent(msg)
-    if intent:
-        return random.choice(intent['responses'])
+#  Tổng hợp các chức năng và để chatbot hiện thị với người dùng
+def chatbot_response(msg, data_conversation):
     
+    # Chức năng in ra tin tức theo đúng chủ đề chọn
+    categories = [
+        "Thời sự", "Góc nhìn", "Thế giới", "Podcasts", "Kinh doanh", "Bất động sản", "Khoa học", 
+                  "Giải trí", "Thể thao", "Pháp luật", "Giáo dục", "Sức khỏe", "Đời sống", "Du lịch", 
+        "Số hóa", "Xe", "Ý kiến", "Tâm sự", "Thư giãn"
+    ]
     selected_category = None
     for category in categories:
         if category.lower() in msg.lower():
             selected_category = category
             break
-
     news_responses = []
 
     if selected_category:
@@ -90,6 +111,7 @@ def chatbot_response(msg):
 
         return "".join(news_responses)
 
+    # In ra tin tức chung 
     elif 'tin tức' in msg.lower():
         random.shuffle(news_scrap_data['intents'])  # Shuffle the news list
         news = news_scrap_data['intents'][0]
@@ -98,7 +120,7 @@ def chatbot_response(msg):
         link = news['news_link']
         return f"<strong>Tiêu đề:</strong> {title}<br> \n <strong>Nội dung:</strong> {summary}<br> \n <strong>Chủ đề:</strong> {news['category']}<br> \n <a href='{link}' target='_blank'>Xem thêm</a><br><br>"
     
-    # code xác định chủ đề câu hỏi
+    # In ra Chủ đề của câu hỏi người dùng nhập vào
     elif 'chủ đề của câu hỏi:' in msg.lower():
         # Tách đoạn văn bản khỏi câu hỏi
         content = msg.lower().replace('chủ đề của câu hỏi:', '').strip()
@@ -106,6 +128,18 @@ def chatbot_response(msg):
             return classify_intent_questions(content)
         return "Vui lòng nhập đoạn văn bản cần phân loại."
       
+    # Hiển thị hội thoại với người dùng 
+    msg = msg.translate(str.maketrans('', '', string.punctuation)).lower() # tiền xử lý dữ liệu 
+    
+    # Tìm input rồi in hội thoại tương ứng ra.
+    for tag, patterns in data_conversation.items():
+        for pattern in patterns:
+            if pattern['Pattern'] == msg:
+                return pattern['Response']
+
+    # Nếu không thấy hội thoại i hệt và tương ứng thì sẽ in ra hội thoại được training
+    return get_response_from_model(msg, model_conver, vectorizer_conver, intents)
+
 app = Flask(__name__)
 app.static_folder = 'static'
 
@@ -116,8 +150,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return chatbot_response(userText)
+    return chatbot_response(userText, data_conversation)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
